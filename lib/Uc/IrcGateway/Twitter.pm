44105caa161aa401dcc3bcaaabdb6c7d105fe167
U=Cormorant <u@chimata.org>
2011-08-13 19:17:16 +0900

    Moosenized
    -utig.pl のパッケージ部分を lib/Uc/IrcGateway/Twitter.pm へ移動
    -TypableMap.pm の Moose 化
    -TypableMap.pm の Tie::Array 化
    -TIMELINEとTypableMapの紐付け成功
    
    # つかれた…

--- lib/Uc/IrcGateway/Twitter.pm
+++ lib/Uc/IrcGateway/Twitter.pm
@@ -1,19 +1,357 @@
 package Uc::IrcGateway::Twitter;
 
-use warnings;
-use strict;
-use Carp;
+use 5.010;
+use common::sense;
+use warnings qw(utf8);
+use Encode qw(decode find_encoding);
+use Any::Moose; # qw(::Util::TypeConstraints);
+use Net::Twitter::Lite;
+use AnyEvent::Twitter::Stream;
+use AnyEvent::Handle;
+use AnyEvent::Socket;
+use AnyEvent::IRC::Util qw/parse_irc_msg mk_msg/;
+use Sys::Hostname;
+use Data::Dumper;
+use Config::Pit;
+use HTML::Entities qw(decode_entities);
+use Uc::IrcGateway::Util::TypableMap;
+use Smart::Comments;
 
-use version; $VERSION = qv('0.0.3');
+use Readonly;
+Readonly my $CHARSET => 'utf8';
 
-# Other recommended modules (uncomment to use):
-#  use IO::Prompt;
-#  use Perl6::Export;
-#  use Perl6::Slurp;
-#  use Perl6::Say;
+our $VERSION = '0.0.3';
+our $CRLF = "\015\012";
+my  $encode = find_encoding($CHARSET);
+
+BEGIN {
+    no strict 'refs';
+    while (my ($code, $name) = each %AnyEvent::IRC::Util::RFC_NUMCODE_MAP) {
+        *{"${name}"} = sub () { $code };
+    }
+};
 
 
-# Module implementation here
+extends 'Object::Event';
+has 'host' => ( is  => 'ro', isa => 'Str', required => 1, default => '127.0.0.1' );
+has 'port' => ( is  => 'ro', isa => 'Int', required => 1, default => 16668 );
+has 'servername' => ( is  => 'rw', isa => 'Str', required => 1, default => sub { hostname() } );
+has 'welcome'    => ( is  => 'rw', isa => 'Str', default => 'welcome to the utig server' );
+has 'conf_app'   => ( is  => 'rw', isa => 'HashRef', required => 1, default => sub { pit_get('utig.pl'); } );
+has 'ctime' => ( is  => 'rw', isa => 'Str' );
+
+__PACKAGE__->meta->make_immutable;
+no Any::Moose;
+
+
+sub BUILD {
+    my $self = shift;
+    $self->reg_cb(
+        nick => sub {
+            my ($self, $msg, $handle) = @_;
+            my $nick = shift @{$msg->{params}};
+
+            unless ($nick) {
+                $self->need_more_params($handle, 'NICK');
+            }
+
+            ### $nick
+            $handle->{conf_user} = pit_get("utig.pl.$nick") if $nick;
+
+            twitter_agent($handle, $self->conf_app, $handle->{conf_user});
+            $handle->{channels}->{'#twitter'} = {};
+            $self->streamer(
+                handle          => $handle,
+                consumer_key    => $self->conf_app->{consumer_key},
+                consumer_secret => $self->conf_app->{consumer_secret},
+                token           => $handle->{conf_user}{token},
+                token_secret    => $handle->{conf_user}{token_secret},
+            );
+        },
+        user => sub {
+            my ($self, $msg, $handle) = @_;
+            my ($nick, $host, $server, $realname) = @{$msg->{params}};
+            $handle->{nick}     = $nick;
+            $handle->{host}     = $host;
+            $handle->{server}   = $server;
+            $handle->{realname} = $realname;
+
+            $handle->{channels}->{'#twitter'} = { $handle->{conf_user}{user_id} => $handle->{nick} };
+            $self->send_msg( $handle, RPL_WELCOME, $self->{welcome} );
+            $self->send_msg( $handle, RPL_YOURHOST, "Your host is @{[ $self->servername ]} [@{[ $self->servername ]}/@{[ $self->port ]}]. @{[ ref $self ]}/$VERSION" ); # 002
+            $self->send_msg( $handle, RPL_CREATED, "This server was created $self->{ctime}");
+            $self->send_msg( $handle, RPL_MYINFO, "@{[ $self->servername ]} @{[ ref $self ]}-$VERSION" ); # 004
+            $self->send_msg( $handle, ERR_NOMOTD, "MOTD File is missing" );
+
+            $self->handle_msg(parse_irc_msg('JOIN #twitter'), $handle);
+        },
+        join => sub {
+            my ($self, $msg, $handle) = @_;
+            my $chans = shift @{$msg->{params}};
+            my $nick = $handle->{nick};
+
+            unless ($chans) {
+                $self->need_more_params($handle, 'JOIN');
+            }
+
+            for my $chan (split /,/, $chans) {
+                my $raw;
+                $handle->{channels}->{$chan}->{$handle->{conf_user}{user_id}} = $nick;
+
+                # sever reply
+                $self->send_msg( $handle, RPL_TOPIC, $chan,  $handle->{topics}->{$chan} || '' );
+                $self->send_msg( $handle, RPL_NAMREPLY, $chan, "duke" ); # TODO
+                $raw = mk_msg($self->servername, 'MODE', $chan, '+o', $nick) . $CRLF;
+                ### $raw
+                $handle->push_write($raw);
+
+                # send join message
+                my $comment = sprintf("%s!%s@%s", $nick, $nick, $self->servername);
+                $raw = mk_msg($comment, 'JOIN', $chan) . $CRLF;
+                ### $raw
+                $handle->push_write($raw);
+            }
+        },
+        part => sub {
+            my ($self, $msg, $handle) = @_;
+            my ($chans, $text) = @{$msg->{params}};
+            my $nick = $handle->{nick};
+
+            unless ($chans) {
+                $self->need_more_params($handle, 'JOIN');
+            }
+
+            for my $chan (split /,/, $chans) {
+                delete $handle->{channels}->{$chan}->{$handle->{conf_user}{user_id}};
+
+                # send part message
+                my $comment = sprintf("%s!%s@%s", $nick, $nick, $self->servername);
+                my $raw = mk_msg($comment, 'PART', $chan, $text) . $CRLF;
+                ### $raw
+                $handle->push_write($raw);
+            }
+        },
+        topic => sub {
+            my ($self, $msg, $handle) = @_;
+            my ($chan, $topic) = @{$msg->{params}};
+            my $nick = $handle->{nick};
+
+            unless ($chan) {
+                $self->need_more_params($handle, 'TOPIC');
+            }
+
+            if ($topic) {
+                $handle->{topics}->{$chan} = $topic;
+                $self->send_msg($handle, RPL_TOPIC, $chan, $topic);
+            }
+            else {
+                $self->send_msg($handle, RPL_NOTOPIC, $chan, 'No topic is set');
+            }
+        },
+        privmsg => sub {
+            my ($self, $msg, $handle) = @_;
+            my ($chan, $text) = @{$msg->{params}};
+            my $nick = $handle->{nick};
+
+            unless ($chan) {
+                $self->need_more_params($handle, 'PRIVMSG');
+            }
+            eval { twitter_agent($handle)->update($encode->decode($text)); };
+            if ($@) {
+                my $comment = sprintf("%s!%s@%s", 'twitterircgateway', 'twitterircgateway', $self->servername);
+                my $raw = mk_msg($comment, 'NOTICE', '#twitter', qq|send error: "$text": $@| ) . $CRLF;
+                $handle->push_write($raw);
+            }
+        },
+        notice => sub {
+            my ($self, $msg, $handle) = @_;
+            my ($chan, $text) = @{$msg->{params}};
+            my $nick = $handle->{nick};
+            unless ($chan) {
+                $self->need_more_params($handle, 'NOTICE');
+            }
+            # no reply any message
+        },
+        list => sub {
+            my ($self, $msg, $handle) = @_;
+            my $chans = shift @{$msg->{params}};
+            my $nick = $handle->{nick};
+            $self->list($handle, $chans);
+        },
+        who => sub {
+            my ($self, $msg, $handle) = @_;
+            my $chans = shift @{$msg->{params}};
+            my $nick = $handle->{nick};
+            unless ($chans) {
+                $self->need_more_params($handle, 'WHO');
+            }
+            while (my ($k, $v) = each %{$handle->{channels}{$chans}}) {
+                $self->send_msg( $handle, RPL_WHOREPLY, $chans, $v, $k, $k, $v, "H :1", $k);
+            }
+            $self->send_msg( $handle, RPL_ENDOFWHO, 'END of /WHO List');
+        },
+        quit => sub {
+            my ($self, $msg, $handle) = @_;
+            undef $handle->{streamer};
+            undef $handle;
+        },
+        on_eof => sub {
+            my ($self, $handle) = @_;
+            undef $handle;
+        },
+    );
+}
+
+sub run {
+    my $self = shift;
+    $self->ctime(scalar(localtime));
+    tcp_server $self->host, $self->port, sub {
+        my ($fh, $host, $port) = @_;
+        my $handle = AnyEvent::Handle->new(fh => $fh,
+            on_error => sub {
+                my $handle = shift;
+                $self->event('on_error', $handle);
+            },
+            on_eof => sub {
+                my $handle = shift;
+                $self->event('on_eof', $handle);
+            },
+        );
+        $handle->on_read(sub { $handle->push_read(line => sub {
+            my ($handle, $line, $eol) = @_;
+            ### $line
+            my $msg = parse_irc_msg($line);
+            ### $msg
+            $self->handle_msg($msg, $handle);
+        }) });
+    }, sub {
+        my ($fh, $host, $port) = @_;
+        say "bound to $host:$port";
+        say $self->welcome();
+    };
+}
+
+sub handle_msg {
+    my ($self, $msg, $handle) = @_;
+    my $event = lc($msg->{command});
+       $event =~ s/^(\d+)$/irc_$1/g;
+    $self->event($event, $msg, $handle);
+}
+
+sub _server_comment {
+    my ($self, $nick) = @_;
+    return sprintf '%s!~%s@%s', $nick, $nick, $self->servername;
+}
+
+sub list {
+    my ($self, $handle, $chans) = @_;
+    my $nick = $handle->{nick};
+    my $comment = $self->_server_comment($nick);
+    my $send = sub {
+        my $msg = mk_msg($comment, @_) . $CRLF;
+        $handle->push_write($msg);
+    };
+    my $send_rpl_list = sub {
+        my $chan = shift;
+        $send->(RPL_LIST, $nick, $chan, scalar values %{$handle->{channels}{$chan}}, (":$handle->{topics}{$chan}" || ''));
+    };
+    $send->(RPL_LISTSTART, $nick, 'Channel', ':Users', 'Name');
+    $chans = join ',', sort keys %{$handle->{channels}} if !$chans;
+    for my $chan (split /,/, $chans) {
+        $send_rpl_list->($chan);
+    }
+    $send->(RPL_LISTEND, '$nick', 'END of /List');
+}
+
+sub send_msg {
+    my ($self, $handle, $cmd, @args) = @_;
+    my $msg = mk_msg($self->host, $cmd, $handle->{nick}, @args) . $CRLF;
+    ### $msg
+    $handle->push_write($msg);
+}
+
+sub need_more_params {
+    my ($self, $handle, $cmd) = @_;
+    $self->send_msg($handle, ERR_NEEDMOREPARAMS, $cmd, 'Not enough parameters');
+}
+
+sub twitter_agent {
+    my ($handle, $conf_app, $conf_user) = @_;
+    return $handle->{nt} if ref $handle->{nt} eq 'Net::Twitter::Lite';
+
+    my $nt = Net::Twitter::Lite->new(%$conf_app);
+    $nt->access_token($conf_user->{token});
+    $nt->access_token_secret($conf_user->{token_secret});
+
+    my ($pin, @userdata);
+    while (!$nt->authorized()) {
+        say 'please open the following url and allow this app, then enter PIN code.';
+        say $nt->get_authorization_url();
+        print 'PIN: '; chomp($pin = <STDIN>);
+
+        @{$conf_user}{qw/token token_secret user_id screen_name/} = $nt->request_access_token(verifier => $pin);
+        $nt->{config_updated} = 1;
+    }
+
+    return $handle->{nt} = $nt;
+}
+
+sub streamer {
+    my ($self, %config) = @_;
+    my $handle = delete $config{handle};
+    my $tmap = tie my(@TIMELINE), 'Uc::IrcGateway::Util::TypableMap', shuffled => 1;
+    return $handle->{streamer} if exists $handle->{streamer};
+    $handle->{streamer} = AnyEvent::Twitter::Stream->new(
+        method  => 'userstream',
+        timeout => 45,
+        %config,
+
+        on_connect => sub {
+            my $comment = sprintf("%s!%s@%s", 'twitterircgateway', 'twitterircgateway', $self->servername);
+            my $raw = mk_msg($comment, 'NOTICE', '#twitter', 'streamer start to read.' ) . $CRLF;
+            $handle->push_write($raw);
+        },
+        on_tweet => sub {
+            my $tweet = shift;
+            my $nick = $tweet->{user}{screen_name};
+            return unless $nick and $tweet->{text};
+
+            (my $text = $encode->encode(decode_entities($tweet->{text}))) =~ s/[\r\n]+/ /g;
+            if (exists $handle->{channels}{'#twitter'} and exists $tweet->{user}{id}) {
+                if (not exists $handle->{channels}{'#twitter'}{$tweet->{user}{id}}) {
+                    my $raw = mk_msg($nick, 'JOIN', '#twitter') . $CRLF;
+                    ### $raw
+                    $handle->push_write($raw);
+                    $handle->{channels}{'#twitter'}{$tweet->{user}{id}} = $nick;
+                }
+                elsif ($handle->{channels}{'#twitter'}{$tweet->{user}{id}} ne $nick) {
+                    my $raw = mk_msg($handle->{channels}{'#twitter'}{$tweet->{user}{id}}, 'NICK', $nick) . $CRLF;
+                    ### $raw
+                    $handle->push_write($raw);
+                    $handle->{channels}{'#twitter'}{$tweet->{user}{id}} = $nick;
+                }
+                if ($nick eq $handle->{nick}) {
+                    $handle->{topics}->{'#twittter'} = "$text [$tmap]";
+                    $self->send_msg($handle, RPL_TOPIC, '#twitter', "$text [$tmap]");
+                }
+                my $comment = sprintf("%s!%s@%s", $nick, $nick, $self->servername);
+                my $raw = mk_msg($comment, 'PRIVMSG', '#twitter', "$text [$tmap]" ) . $CRLF;
+                # $raw
+                $handle->push_write($raw);
+                push @TIMELINE, $tweet if defined $tweet;
+            }
+        },
+        on_error => sub {
+            warn "error: $_[0]";
+            #        undef $streamer;
+        },
+        on_eof => sub {
+            my $comment = sprintf("%s!%s@%s", 'twitterircgateway', 'twitterircgateway', $self->servername);
+            my $raw = mk_msg($comment, 'NOTICE', '#twitter', 'streamer stop to read.' ) . $CRLF;
+            $handle->push_write($raw);
+        },
+    );
+}
 
 
 1; # Magic true value required at end of module
--- lib/Uc/IrcGateway/Util/TypableMap.pm
+++ lib/Uc/IrcGateway/Util/TypableMap.pm
@@ -4,88 +4,241 @@ use common::sense;
 use warnings qw(utf8);
 use Carp qw(croak);
 use List::Util qw(shuffle);
-use overload
-    '++' => \&increment,
-    '--' => \&decrement,
-    '""' => \&current,
-    '='  => \&current,
-;
+use overload '""' => \&tid, '=' => \&tid;
 use Smart::Comments;
+use Any::Moose;
 
-our @TYPABLE_MAP;
+=ignore
+methods:
+    ITEM     = pop()
+    ITEMS    = push( ITEM [, ITEM, ...] )
+    ITEM     = shift()
+    ITEMS    = unshift( ITEM [, ITEM, ...] )
+    ITEMS    = get( TID [, TID, TID, ...] )
+    ITEMS    = set( TID => ITEM [, TID => ITEM] )
+    BOOL     = exists( TID )
+    ITEMS    = delete( TID [, TID, TID, ...] )
+    NUMBER   = size( [ MAXSIZE ] ) # get or set array size
+    ITEMS    = splice( OFFSET, LENGTH [, ITEM [, ITEM, ITEM, ...]] ) # return DELETE_ITEMS
+    NUMBER   = index() # current index number
+    TID      = tid()   # current tid
+    NUMBER   = tid2index( TID ) # index number of tid
+    TID      = index2tid( NUMBER ) # tid of index number
+
+properties:
+    items   -> { TID => ITEM, ... } # hash as item storage
+    indices -> [ TID, TID, ... ]    # list of tid
+    chars   -> [ CHAR, CHAR, ... ]  # chars list for tid
+    index   -> NUMBER               # number of current index
+
+options:
+    scale    -> NUMBER   # number of digits of tid
+    fixed    -> BOOL     # use fixed digit number for tid
+    chars    -> ARRAYREF # chars list for tmap
+    shuffled -> BOOL     # randomize tmap index
+
+=cut
+
+
+our @TYPABLE_MAP = ();
 my @consonant_voiceless         = qw/k s t n h m y r w/;
 my @consonant_sonant            = qw/g z d b p/;
 my @consonant_contracted        = qw/ky sh ty ny hy py my ry/;
 my @consonant_contracted_sonant = qw/gy zy dy by py/;
 my @consonant = (@consonant_voiceless, @consonant_contracted, @consonant_sonant, @consonant_contracted_sonant);
-for my $consonant ('', @consonant) {
-    my @vowel;
-    given ($consonant) {
-        when ([@consonant_contracted, @consonant_contracted_sonant]) { @vowel = qw/a u o/;     }
-        default                                                      { @vowel = qw/a i u e o/; }
+
+has 'index' => ( is => 'rw', isa => 'Int', default => 0 );
+has 'scale' => ( is => 'ro', isa => 'Int', default => 2 );
+has [qw/fixed shuffled/] => ( is => 'ro', isa => 'Bool', default => 0 );
+has 'max_size'  => ( is => 'rw', isa => 'Int', lazy_build => 1, trigger => sub {
+    my ($self, $value) = @_;
+    my $max_size = scalar @{$self->indices};
+    if ($value > $max_size) { $self->max_size($max_size); }
+    else { CORE::splice @{$self->indices}, $value; }
+} );
+
+has 'items'   => ( is => 'ro', isa => 'HashRef', default => sub { return {} } );
+has 'indices' => ( is => 'ro', isa => 'ArrayRef[Str]', lazy_build => 1 );
+has 'chars'   => ( is => 'ro', isa => 'ArrayRef[Str]', default => sub {
+    return \@TYPABLE_MAP if scalar @TYPABLE_MAP;
+    local $_;
+    for my $consonant ('', @consonant) {
+        my @vowel;
+        given ($consonant) {
+            when ([@consonant_contracted, @consonant_contracted_sonant]) { @vowel = qw/a u o/;     }
+            default                                                      { @vowel = qw/a i u e o/; }
+        }
+        push @TYPABLE_MAP, "$consonant$_" for @vowel;
     }
-    push @TYPABLE_MAP, "$consonant$_" for @vowel;
-}
+    return \@TYPABLE_MAP;
+} );
 
-sub new {
-    my $class  = shift;
-    my $self   = bless {}, __PACKAGE__;
-    my %config = @_;
+__PACKAGE__->meta->make_immutable;
+no Any::Moose;
 
-    $self->{index} = 0;
-    $self->{tid}   = {};
-    $self->{scale} = delete $config{scale} || 2;
-    $self->{chars} = delete $config{chars} || \@TYPABLE_MAP;
-    $self->{fixed} = delete $config{fixed} || 0;
-    $self->{current} = delete $config{init_index} || 0;
-    $self->{shuffled_tmap}  = delete $config{shuffled_tmap} || 0;
-    $self->{auto_increment} = delete $config{auto_increment} || 0;
-    $self->{auto_decrement} = delete $config{auto_decrement} || 0;
+sub _build_indices {
+    my $self = CORE::shift;
+    my @indices; my $a = $self->chars;
 
-    my @tmap;
     my $fill_tmap; $fill_tmap = sub {
-        my ($tmap, $chars, $prefix, $level) = @_;
-        for my $tid (@$chars) {
-            if ($level) { $fill_tmap->($tmap, $chars, "$prefix$tid", $level-1); }
-            else { push @$tmap, "$prefix$tid"; }
+        my ($prefix, $level) = @_;
+        for my $str (@{$self->chars}) {
+            if ($level) { $fill_tmap->("$prefix$str", $level-1); }
+            else { CORE::push @indices, "$prefix$str"; }
         }
     };
-    for my $level (0 .. $self->{scale}-1) {
-        next if $self->{fixed} && $level != $self->{scale}-1;
-        $fill_tmap->(\@tmap, $self->{chars}, '', $level);
-    }
-    $self->{chars} = \@tmap;
-
-    if ($self->{shuffled_tmap}) {
-        @{$self->{chars}} = shuffle @{$self->{chars}};
-    }
-    if ($self->{auto_increment} && $self->{auto_decrement}) {
-        croak "should not set 'auto_incriment' and 'auto_decriment' together";
+    for my $level (0 .. $self->scale-1) {
+        next if $self->fixed && $level != $self->scale-1;
+        $fill_tmap->('', $level);
     }
 
-    return $self;
+    @indices = shuffle @indices if $self->shuffled;
+
+    return \@indices;
 }
 
-sub get       { $_[0]->{tid}{$_[1]} }
-sub index     { $_[0]->{index} }
-sub max_size  { scalar @{$_[0]->{chars}} }
-sub increment { $_[0]->{_c} =  1; $_[0]->current }
-sub decrement { $_[0]->{_c} = -1; $_[0]->current }
-sub current   {
-    my ($self, $tid) = shift;
-    $self->{index} += delete $self->{_c} if exists $self->{_c};
-    $tid = $self->burn;
-    $self->{index}++ if  $self->{auto_increment} && !$self->{auto_decrement};
-    $self->{index}-- if !$self->{auto_increment} &&  $self->{auto_decrement};
-    $self->burn if $self->{auto_increment} || $self->{auto_decrement};
-    return $tid || undef;
+sub _build_max_size {
+    return scalar @{+CORE::shift->indices};
 }
-sub burn {
-    my ($self, $tid) = shift;
-    $self->{current} = $self->index % $self->max_size;
-    $tid = $self->{chars}[$self->{current}];
-    $self->{tid}{$tid} = $self->index;
-    return $tid;
+
+
+# methods:
+
+sub get {
+    my $self = CORE::shift;
+    return wantarray ? @{$self->items}{@_} : ${$self->items}{+CORE::shift};
+}
+sub set {
+    my $self = CORE::shift;
+    my %set  = @_;
+    while (my($k, $v) = each %set) {
+        $self->items->{$k} = $v;
+    }
+    return values %set;
+}
+sub tid {
+    my ($self, $index) = @_;
+    $index = 0 if !$index || $index !~ /^[+-]?\d+$/;
+    $a = $self->index;
+    $b = $self->indices->[$self->index+$index];
+    return $self->indices->[$self->index+$index];
+}
+sub index2tid { ${$_[0]->indices}[$_[1]]; }
+sub tid2index {
+    my ($self, $tid) = @_;
+    my $i = 0;
+    for my $index (@{$self->indices}) {
+        last if $index eq $tid; $i++;
+    }
+    return $i;
+}
+sub roll_index {
+    my ($self, $reverse) = @_;
+    if ($reverse) { CORE::unshift @{$self->indices}, CORE::pop   @{$self->indices}; }
+    else          { CORE::push    @{$self->indices}, CORE::shift @{$self->indices}; }
+}
+sub pop {
+    my $self = CORE::shift;
+    my $item = $self->delete($self->tid);
+    $self->index($self->index-1) if defined $item;
+    return $item;
+}
+sub shift {
+    my $self = CORE::shift;
+    my $item = $self->delete($self->indices->[0]);
+    $self->roll_index if defined $item;
+    return $item;
+}
+sub push {
+    my ($self, @items) = @_;
+    my %set;
+    for my $item (@items) {
+        $set{$self->tid} = $item;
+        if ($self->index < $self->max_size) {
+            $self->index($self->index+1);
+        }
+        else {
+            $self->roll_index;
+        }
+    }
+    $self->set(%set);
+}
+sub unshift {
+    my ($self, @items) = @_;
+    my %set;
+    for my $item (@items) {
+        $set{$self->tid} = $item;
+        if ($self->index < $self->max_size) {
+            $self->index($self->index+1);
+        }
+        else {
+            $self->roll_index(1);
+        }
+    }
+    $self->set(%set);
+}
+sub splice {
+    # TODO: Ç–Ç«Ç¢ÅcÇ¢Ç¬Ç©íºÇ∑
+
+    my ($self, $offset, $length, @items) = @_;
+    my $endset = $offset + $length - 1;
+
+    croak "illegal offset was set" if $offset < 0 || $offset > $self->max_size;
+    croak "illegal length was set" if $endset < 0 || $endset > $self->max_size;
+
+    my @tids = @{$self->indices}[$offset .. $endset];
+    my @delete_items = $self->get(@tids);
+    my $diff = scalar @items - scalar @delete_items;
+    if ($diff > 0) {
+        for my $i (0 .. $diff) {
+            if ($self->index < $self->max_size) {
+                $self->index($self->index+1);
+                my ($tid) = CORE::splice @{$self->indices}, $self->index, 1;
+                CORE::splice @{$self->indices}, $offset+$i, 0, $tid;
+                CORE::push @tids, $tid;
+            }
+            elsif ($offset <= 0) {
+                $self->roll_index(1);
+                my ($tid) = CORE::shift @{$self->indices};
+                CORE::splice @{$self->indices}, $i+$offset, 0, $tid;
+                CORE::push @tids, $tid;
+            }
+            else {
+                $self->roll_index;
+                my ($tid) = CORE::pop @{$self->indices};
+                CORE::splice @{$self->indices}, $i+$offset--, 0, $tid;
+                CORE::push @tids, $tid;
+            }
+        }
+    }
+    @delete_items = $self->get(@tids);
+    my %set = map { ($tids[$_] => $items[$_]) } 0 .. $#tids;
+    $self->set(%set);
+
+    return @delete_items;
+}
+sub exists { CORE::exists $_[0]->items->{$_[1]}; }
+sub delete { CORE::delete @{+CORE::shift->items}{@_}; }
+sub size   { scalar values %{+CORE::shift->items} }
+
+
+# for tie
+
+use Tie::Array;
+
+sub TIEARRAY  { CORE::shift->new(@_); }
+sub STORE     { $_[0]->set(${$_[0]->indices}[$_[1]] => $_[2]); }
+sub FETCH     { $_[0]->get($_[0]->tid($_[1]-$_[0]->index)); }
+sub FETCHSIZE { $_[0]->size; }
+sub EXISTS    { $_[0]->exists($_[1]); }
+sub DELETE    { $_[0]->delete($_[1]); }
+sub PUSH      { CORE::shift->push(@_); }
+sub POP       { $_[0]->pop; }
+sub SHIFT     { $_[0]->shift; }
+sub UNSHIFT   { CORE::shift->unshift(@_); }
+sub SPLICE    {
+    my ($self, $offset, $length, @item) = @_;
+    $self->splice($offset, $length, @item);
 }
 
 
@@ -101,13 +254,23 @@ Uc::IrcGateway::Util::TypableMap - [Generate TypableMap Object for Uc::IrcGatewa
 
     use Uc::IrcGateway::Util::TypableMap;
 
-    my @TIMELINE;
-    my $tmap = Uc::IrcGateway::Util::TypableMap->new(auto_increment => 1);
-    push @TIMELINE, "you suck! [$tmap]"; # you suck! [a]
-    push @TIMELINE, "you suck too! [$tmap]"; # you suck too! [i]
-    push @TIMELINE, "there is awesome internet! [$tmap]"; # there is awesome internet! [u]
+    my $tmap = Uc::IrcGateway::Util::TypableMap->new(scale => 1);
+    $tmap->push("you suck! [$tmap]"); # you suck! [a]
+    $tmap->push("you suck too! [$tmap]"); # you suck too! [i]
+    $tmap->push("there is awesome internet! [$tmap]"); # there is awesome internet! [u]
 
-    some_code($TIMELINE[$tmap->get('u')]); # some_code("there is awesome internet! [u]");
+    print $tmap->get('u'); # there is awesome internet! [u];
+
+    # or
+    
+    my @TIMELINE;
+    my $tmap = tie \@TIMELINE, 'Uc::IrcGateway::Util::TypableMap', fixed => 1, shuffled => 1;
+    push @TIMELINE, "you suck! [$tmap]"; # you suck! [ka]
+    push @TIMELINE, "you suck too! [$tmap]"; # you suck too! [nu]
+    push @TIMELINE, "there is awesome internet! [$tmap]"; # there is awesome internet! [pia]
+
+    print $TIMELINE[2]; # there is awesome internet! [pia];
+    print $TIMELINE[$tmap->tid2index('pia')]; # there is awesome internet! [pia];
 
 
 =head1 DESCRIPTION
